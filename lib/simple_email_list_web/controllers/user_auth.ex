81ef60b336896690ee2d8aa9f1b1c2d89d742b3b
mellkior <38818928+mellkior@users.noreply.github.com>
2022-03-24 14:46:15 -0400

    updated default signed in path

--- lib/simple_email_list_web/controllers/user_auth.ex
+++ lib/simple_email_list_web/controllers/user_auth.ex
@@ -145,5 +145,5 @@ defmodule SimpleEmailListWeb.UserAuth do
 
   defp maybe_store_return_to(conn), do: conn
 
-  defp signed_in_path(_conn), do: "/"
+  defp signed_in_path(conn), do: Routes.list_path(conn, :index)
 end

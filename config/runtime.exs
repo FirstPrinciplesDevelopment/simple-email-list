55634df09b295fd2297cb21f7b41b26fe57b975b
mellkior <38818928+mellkior@users.noreply.github.com>
2022-11-10 12:48:47 -0500

    added required ApiClient config

--- config/runtime.exs
+++ config/runtime.exs
@@ -82,9 +82,13 @@ if config_env() == :prod do
   #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
   #
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+
 # Sendgrid configuration.
 config :simple_email_list, SimpleEmailList.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
   api_key: System.get_env("SENDGRID_API_KEY")
+
+# Configuring an ApiClient is required for non-SMTP Swoosh.Adapters.
+config :swoosh, :api_client, Swoosh.ApiClient.Hackney
 
 end
